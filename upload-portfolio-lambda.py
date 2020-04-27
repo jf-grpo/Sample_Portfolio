ac1bdcbe55fd6b82738a6202ab52149b3addf59d
Joe Reynolds <j7f7r7@gmail.com>
2020-04-27 18:10:15 -0500

    Use SNS topic to notify me when portfolio is deployed.

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,23 +5,30 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:470922430702:deployPortfolioNotice')
     
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+        
+        
+        portfolio_bucket = s3.Bucket('portfolio.jftestzone.org')
+        build_bucket = s3.Bucket('portfoliobuild.jftestzone.org')
+        
+        portfolio_zip = io.BytesIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                portfolio_bucket.upload_fileobj(obj, nm,
+                  ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+                portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        
+        topic.publish(Subject="Your Portfolio", Message="Portfolio Deployed Successfully")
+    except:
+        topic.publish(Subject="Your Portfolio", Message="Portfolio did no deploy successfully.")
+        raise
     
-    portfolio_bucket = s3.Bucket('portfolio.jftestzone.org')
-    build_bucket = s3.Bucket('portfoliobuild.jftestzone.org')
+        return 'Hello from Lambda'
     
-    portfolio_zip = io.BytesIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-    
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            portfolio_bucket.upload_fileobj(obj, nm,
-              ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    
-    return 'Hello from Lambda'
-
-
