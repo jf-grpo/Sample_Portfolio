cf49ceb62019ec536db3c3100aec55a851d6b3cb
Joe Reynolds <j7f7r7@gmail.com>
2020-05-02 13:32:34 -0500

    Removed image, corrected Lambda code, updated index.html and upload-portfolio-lambda.py

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -40,7 +40,7 @@ def lambda_handler(event, context):
         topic.publish(Subject="Your Portfolio", Message="Portfolio Deployed Successfully")
         if job:
             codepipeline = boto3.client('codepipeline')
-            codepipeline.put_job_success_result(jobID=job['id'])
+            codepipeline.put_job_success_result(jobId=job['id'])
             
     except:
         topic.publish(Subject="Your Portfolio", Message="Portfolio did no deploy successfully.")
