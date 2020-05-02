3e38d0ea1a06e0d79a9d5ea3bc620741bc3348e7
Joe Reynolds <j7f7r7@gmail.com>
2020-05-02 13:54:05 -0500

    Changed pipeline name from Portfolio to PortfolioBuild

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -17,7 +17,7 @@ def lambda_handler(event, context):
         
         if job:
             for artifact in job["data"]["inputArtifacts"]:
-                if artifact["name"] == "Portfolio":
+                if artifact["name"] == "PortfolioBuild":
                     location = artifact["location"]["s3Location"]
                     
         print("Building portfolio from " + str(location))
